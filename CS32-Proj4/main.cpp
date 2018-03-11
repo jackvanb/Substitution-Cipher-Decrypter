ce5091c9303f4824ed5e5c88e0865f5cc1139876
jackvanboening <jackvanboening@gmail.com>
2018-03-10 16:03:39 -0800

    Initial Commit

---
+++ CS32-Proj4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS32-Proj4
+//
+//  Created by Jack Van Boening on 3/10/18.
+//  Copyright © 2018 Jack Van Boening. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
